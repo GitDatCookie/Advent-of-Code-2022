d2a12e120ef0ea6eef369f1b36be506c868abddd
GitDatCookie <fatestayapple@gmail.com>
2022-12-06 23:43:21 +0100

    Created Day 4 Part 1

---
+++ Advent of Code 2022/4.Day/CalculateTask4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2022._4.Day
+{
+    internal class CalculateTask4
+    {
+        public int CalculatePart1(string fileLink)
+        {
+            Camp_Cleanup_Part1 part1 = new();
+            string[] campSectionList = part1.GetCampSectionList(fileLink);
+            List<int[]> firstElfSectionList = part1.GetFirstElfSectionList(campSectionList);
+            List<int[]> secondElfSectionList = part1.GetSecondElfSectionList(campSectionList);
+            int result = part1.GetOverlappingSectionNumber(firstElfSectionList, secondElfSectionList);
+            return result;
+        }
+    }
+}
---
+++ Advent of Code 2022/4.Day/Camp_Cleanup_Part1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2022._4.Day
+{
+    internal class Camp_Cleanup_Part1
+    {
+        public string[] GetCampSectionList(string fileLink)
+        {
+            string[] campSectionList = System.IO.File.ReadAllLines(fileLink);
+            return campSectionList;
+        }
+
+        public List<int[]> GetFirstElfSectionList(string[] campSectionList)
+        {
+            List<int[]> firstElfSectionList = new();
+
+            string subString;
+            int firstSection;
+            int lastSection;
+            int[] sections;
+
+            foreach(var campSection in campSectionList)
+            {
+                subString = campSection.Substring(0, campSection.IndexOf(","));
+
+                int.TryParse(subString.Substring(0, subString.IndexOf("-")), out firstSection);
+                int.TryParse(subString.Substring(subString.IndexOf("-")), out lastSection);
+                sections = new int[] { firstSection, lastSection };
+                firstElfSectionList.Add(sections);
+            }
+
+            return firstElfSectionList;
+        }
+
+        public List<int[]> GetSecondElfSectionList(string[] campSectionList)
+        {
+            List<int[]> secondtElfSectionList = new();
+
+            string subString;
+            int firstSection;
+            int lastSection;
+            int[] sections;
+
+            foreach (var campSection in campSectionList)
+            {
+                subString = campSection.Substring(campSection.IndexOf(","));
+
+                int.TryParse(subString.Substring(0, subString.IndexOf("-")), out firstSection);
+                int.TryParse(subString.Substring(subString.IndexOf("-")), out lastSection);
+                sections = new int[] { firstSection, lastSection };
+                secondtElfSectionList.Add(sections);
+            }
+
+            return secondtElfSectionList;
+        }
+
+        public int GetOverlappingSectionNumber(List<int[]> firstElfSectionList, List<int[]> secondElfSectionList)
+        {
+            int overlappingSectionNumber = 0; ;
+
+            for(int sectionNumber = 0; sectionNumber < firstElfSectionList.Count; sectionNumber++)
+            {
+                if ((firstElfSectionList[sectionNumber][0] >= secondElfSectionList[sectionNumber][0] && firstElfSectionList[sectionNumber][1] <= secondElfSectionList[sectionNumber][1]) 
+                    || (secondElfSectionList[sectionNumber][0] >= firstElfSectionList[sectionNumber][0] && secondElfSectionList[sectionNumber][0] <= firstElfSectionList[sectionNumber][0]))
+                {
+                    overlappingSectionNumber += 1;
+                }
+            }
+            return overlappingSectionNumber;
+        }
+    }
+}
